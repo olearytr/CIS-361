10d69f9933d17f31d008f4172443646754ca4e5f
olearytr <oleary54321@gmail.com>
2016-01-24 19:38:48 -0500

    Added code to commented segments

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -11,6 +11,8 @@ int main ()
 	char ch = ' ';
 	int i, j, size = 0;
 
+	char rev[26] = {'z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a'};
+
 	// read the key from the keyboard into array key
 	// (length of the key is less than 20 characters
 	
@@ -20,12 +22,47 @@ int main ()
 	
 
 	// write code below to store the key in array alp 
-	// after removing duplicate lettera
+	// after removing duplicate letters
 	
+	size = 0;
+
+	for (i =0; i < 20; i++){
+	
+		ch=key[i];
+
+		ch = (tolower(ch));
+
+		for (j = 0; j < 26; j++) {
+			
+			//If the char is already in alp, stop 
+			if ( ch == alp[j] ) { break; }
+
+			//If the current spot is not empty, skip it
+			if ( alp[j] != '\0' ) { /* Do nothing */ }
+
+			else {
+				alp[j] = ch;
+				size++;
+				break;	
+			}
+		}
+	}
 
 	// write code below to fill up the rest of array alp wiht
 	// other characters in the alphabet in reverse order
 	
+	for (i = 0; i < 26; i++ ){
+
+		ch = rev[i];
+
+		j = found(alp, 26, ch);
+
+		if ( j == 0) {
+			alp[size] = ch;
+			size++;
+		}
+
+	}
 
 	// print characters in array alp
 	
